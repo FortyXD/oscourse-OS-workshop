d4bc649a0591a30cd29589696d9099f0b770d754
Andrey Moiseykin <andreymoiseykin@Andreys-MacBook-Air.local>
2025-10-07 20:22:45 +0300

    lab 4 done

--- kern/init.c
+++ kern/init.c
@@ -97,7 +97,7 @@ i386_init(void) {
     /* Initialize the console.
      * Can't call cprintf until after we do this! */
     cons_init();
-
+    // assert(false);
     if (trace_init) {
         cprintf("6828 decimal is %o octal!\n", 6828);
         cprintf("END: %p\n", end);
